5174f846195bdb31bd67b80d6eef44d3c7a90f5f
Tyler Potts <tylerpottsdev@gmail.com>
2022-07-07 00:29:25 +0100

    Styling update

--- src/App.vue
+++ src/App.vue
@@ -62,9 +62,9 @@ const SaveGame = () => {
 }
 
 const LoadGame = () => {
-	wins.value = parseInt(localStorage.getItem('wins'))
-	draws.value = parseInt(localStorage.getItem('draws'))
-	losses.value = parseInt(localStorage.getItem('losses'))
+	wins.value = parseInt(localStorage.getItem('wins')) || 0
+	draws.value = parseInt(localStorage.getItem('draws')) || 0
+	losses.value = parseInt(localStorage.getItem('losses')) || 0
 }
 
 const ResetRound = () => {
@@ -75,47 +75,63 @@ const ResetRound = () => {
 
 onMounted(() => {
 	LoadGame()
+
+	window.addEventListener('keypress', (e) => {
+		if (e.key === 'r') {
+			ResetRound()
+		}
+	})
 })
 </script>
 
 <template>
-	<div class="bg-gray-700 text-white min-h-screen flex flex-col">
-		<header class="container mx-auto px-6 py-4">
-			<h1 class="text-4xl font-bold text-center">Rock, Paper, Scissors!</h1>
+	<div class="bg-gray-700 text-white text-center min-h-screen flex flex-col">
+		<header class="container mx-auto p-6">
+			<h1 class="text-4xl font-bold">Rock, Paper, Scissors!</h1>
 		</header>
 
-		<main class="container mx-auto px-6 py-4 flex-1">
+		<main class="container mx-auto p-6 flex-1">
 			<div v-if="choice === null" class="flex items-center justify-center -mx-6">
-				
-				<button @click="play('rock')" class="mx-6">
-					<img src="./assets/RockIcon.svg" alt="Rock" />
+
+				<button @click="play('rock')"
+					class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-pink-500">
+					<img src="./assets/RockIcon.svg" alt="Rock" class="w-full" />
 				</button>
 
-				<button @click="play('paper')" class="mx-6">
+				<button @click="play('paper')"
+					class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-green-500">
 					<img src="./assets/PaperIcon.svg" alt="Paper" />
 				</button>
 
-				<button @click="play('scissors')" class="mx-6">
+				<button @click="play('scissors')"
+					class="bg-white rounded-full shadow-lg w-64 p-12 mx-6 transition-colors duration-300 hover:bg-yellow-500">
 					<img src="./assets/ScissorsIcon.svg" alt="Scissors" />
 				</button>
 
 			</div>
 
-			<div v-else class="verdict">
-				<h2>{{ choice }}</h2>
-				<h2>{{ computerChoice }}</h2>
-				<h2>{{ verdict }}</h2>
+			<div v-else>
+				<div class="text-3xl mb-4">
+					You picked <span class="text-pink-500">{{ choice }}</span>
+				</div>
+				<div class="text-3xl mb-4">
+					The computer picked <span class="text-green-500">{{ computerChoice }}</span>
+				</div>
+				<div class="text-6xl mb-12">
+					{{ verdict }}
+				</div>
 
-				<button @click="ResetRound">Reset</button>
+				<button @click="ResetRound" class="bg-pink-500 text-lg py-2 px-4">Reset</button>
 			</div>
 
-			<div>{{ wins }} : {{ draws }} : {{ losses }}</div>
+			<div class="mt-12 text-3xl mb-4">{{ wins }} : {{ draws }} : {{ losses }}</div>
 
-			<div>Win rate: {{ Math.round(winPercentage) }}%</div>
+			<div class="text-lg">Win rate: {{ Math.round(winPercentage) }}%</div>
 		</main>
 
-		<footer class="container mx-auto px-6 py-4">
-			<a href="https://www.vecteezy.com/free-vector/rock-paper-scissors-game">Rock Paper Scissors Game Vectors by Vecteezy</a>
+		<footer class="container mx-auto p-6">
+			<a href="https://www.vecteezy.com/free-vector/rock-paper-scissors-game">Rock Paper Scissors Game Vectors by
+				Vecteezy</a>
 		</footer>
 	</div>
 </template>
